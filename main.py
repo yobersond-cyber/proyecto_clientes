17ffad80d79f4f26a294c98afe6a48a8e2033bb5
yoberson <yobersond@gmail.com>
2026-06-19 16:27:22 -0500

    buscar por id

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
+
 app= FastAPI ()
- 
-#endpoints 
-
-@app.get ("/proyecto")
-
-def mensaje ():
-    return{"proyecto": "este es el proyecto de clientes a desarrollar"}
-
-
-lista_clientes= ["Ricardo","yoberson","quiroga","daniela","ana","kamila","zuelima,"]
-@app.get ("/clientes")
-def clientes ():
-    return {"clientes": lista_clientes} 
-
-
-
-app = FastAPI()
 
 class Cliente(BaseModel):
     id: int
@@ -36,5 +20,34 @@ class Transaccion(BaseModel):
     metodo_pago: str
     monto: float
     factura_id: int
+
+clientes = []
+facturas = []
+transacciones = []
+
+#endpoints 
+
+@app.get ("/proyecto")
+
+def mensaje ():
+    return{"proyecto": "este es el proyecto de clientes a desarrollar"}
+
+
+lista_clientes= ["Ricardo","yoberson","quiroga","daniela","ana","kamila","zuelima,"]
+@app.get ("/clientes")
+def clientes ():
+    return {"clientes": lista_clientes} 
+
+# Buscar cliente por ID
+
+@app.get("/clientes/{id}")
+def obtener_cliente(id: int):
+    for cliente in clientes:
+        if cliente.id == id:
+            return cliente
+
+    raise HTTPException(404, "Cliente no encontrado")
+
+
 
 
