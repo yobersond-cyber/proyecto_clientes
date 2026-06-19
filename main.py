88ee2d2f1ec050090efb03c3ea10d1a5e69ae49f
yoberson <yobersond@gmail.com>
2026-06-19 16:55:17 -0500

    clases

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app= FastAPI ()
 
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
+
+
+class Factura(BaseModel):
+    id: int
+    cliente_id: int
+    vrtotal: float
+
+
+class Transaccion(BaseModel):
+    id: int
+    vr_unitario: float
+    cantidad: int
+    factura_id: int
+
+
 lista_clientes = [
     {"id": 1, "nombre": "Lady", "email": "lady@gmail.com", "edad": 22, "descripcion": "NA"},
     {"id": 2, "nombre": "Luis", "email": "luis@gmail.com", "edad": 19, "descripcion": "NA"},
