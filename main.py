cb2af6cf33578e17cb3c9fb4f5e1e185e03077f5
yoberson <yobersond@gmail.com>
2026-06-19 16:22:37 -0500

    creacion de clases

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app= FastAPI ()
  
 #endpoints 
@@ -14,5 +14,27 @@ lista_clientes= ["Ricardo","yoberson","quiroga","daniela","ana","kamila","zuelim
 @app.get ("/clientes")
 def clientes ():
     return {"clientes": lista_clientes} 
+
+
+
+app = FastAPI()
+
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    correo: str
+    descripcion: str
+
+class Factura(BaseModel):
+    id: int
+    fecha: str
+    Valortotal: float
+    cliente_id: int
+
+class Transaccion(BaseModel):
+    id: int
+    metodo_pago: str
+    monto: float
+    factura_id: int
 
 
