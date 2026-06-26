80efafcd40b7c99a393c8d981de61f4d813540bd
yobersond-cyber <yobersond@gmail.com>
2026-06-25 21:14:27 -0500

    id automatico

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ lista_clientes: list [cliente] = []
 
 #endpoints 
 @app.get ("/lista de clientes", response_model=list [cliente])
-def listar_clientes ():
+async def listar_clientes ():
     return lista_clientes
 
 #endpoint listar un cliente
 @app.get("/clientes/{cliente_id}", response_model=cliente)
-def listar_cliente(cliente_id: int):
+async def listar_cliente(cliente_id: int):
     for i, obj_client in enumerate (lista_clientes):
         if obj_client.get("id") == cliente_id:
             return obj_client
@@ -19,8 +19,10 @@ def listar_cliente(cliente_id: int):
 
 #endpoint para crear un cliente, y agregarlo a al lista 
 @app.post("/crear cliente", response_model=cliente)
-def crear_cliente(datos_cliente: clientecrear):
+async def crear_cliente(datos_cliente: clientecrear):
     cliente_val = cliente.model_validate(datos_cliente.model_dump()) 
+    id_cliente = len (lista_clientes) +1 
+    cliente_val.id = id_cliente
     lista_clientes.append(cliente_val) 
     return cliente_val
 
